e8c07cdce364dec53939476126fabb8512deff05
Sample <913925047@qq.com>
2017-06-20 00:45:25 +0800

    基础练习

---
+++ 基础类型.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+       int i = 255;
+       float f = 3.14;
+       double d = 3.14159265354;
+       char c = 'f';
+       short s = 123;
+       char str[] = {'h','e','l','l','o','w','o','r','l','d','\0'};
+       printf("int = %d\n",i);
+       printf("float = %f\n",f);
+       printf("double = %d\n",d);
+       printf("char = %c\n",c);
+       printf("short = %d\n",s); 
+       printf("str = %s\n",str);     
+       system("pause");
+       
+}
